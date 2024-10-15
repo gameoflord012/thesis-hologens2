49f1bcbfc3efb981dc3a7b22277c3ef034653eef
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-10-15 15:46:20 +0700

    disable test script

--- src/UnityProject/Assets/_Scenes/Test/SpartialAwarenessTest.cs
+++ src/UnityProject/Assets/_Scenes/Test/SpartialAwarenessTest.cs
@@ -5,11 +5,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using System;
+using Microsoft.MixedReality.Toolkit.SpatialObjectMeshObserver;
 
 public class SpartialAwarenessTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+    {
+        InitMeshObserver();
+        //StartStopMeshObserverTest();
+        EnumMeshes();
+    }
+
+
+
+    private void EnumMeshes()
+    {
+        int count = 0;
+        foreach(SpatialAwarenessMeshObject meshObject in meshObserver.Meshes.Values)
+        {
+            Mesh mesh = meshObject.Filter.mesh;
+            count++;
+        }
+
+        Assert.IsTrue(count > 0);
+    }
+
+    private void StartStopMeshObserverTest()
+    {
+        meshObserver.Suspend();
+        meshObserver.Resume();
+    }
+
+    private void InitMeshObserver()
     {
         // Use CoreServices to quickly get access to the IMixedRealitySpatialAwarenessSystem
         var spatialAwarenessService = CoreServices.SpatialAwarenessSystem;
@@ -17,8 +46,10 @@ public class SpartialAwarenessTest : MonoBehaviour
 
         // Get the SpatialObjectMeshObserver specifically
         var meshObserverName = "Spatial Object Mesh Observer";
-        var spatialObjectMeshObserver = dataProviderAccess.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>(meshObserverName);
+        meshObserver = dataProviderAccess.GetDataProvider<SpatialObjectMeshObserver>(meshObserverName);
 
-        Assert.IsNotNull(spatialObjectMeshObserver);
+        Assert.IsNotNull(meshObserver);
     }
+
+    private SpatialObjectMeshObserver meshObserver;
 }
