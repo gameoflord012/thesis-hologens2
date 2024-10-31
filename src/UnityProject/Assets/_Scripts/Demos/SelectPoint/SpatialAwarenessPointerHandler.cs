5244dd2443f0be105b830feded2335cd9b9f7849
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-10-31 16:36:16 +0700

    spawn point succress

--- src/UnityProject/Assets/_Scripts/Demos/SelectPoint/SpatialAwarenessPointerHandler.cs
+++ src/UnityProject/Assets/_Scripts/Demos/SelectPoint/SpatialAwarenessPointerHandler.cs
@@ -1,37 +1,36 @@
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class SpatialAwarenessPointerHandler : MonoBehaviour
 {
     [SerializeField] private GameObject spawnRoot;
+    [SerializeField] private GameObject spawnPrefab;
 
     void Start()
     {
         Assert.IsNotNull(spawnRoot);
 
         GameObject SARoot = CoreServices.SpatialAwarenessSystem.SpatialAwarenessObjectParent;
-        var pHandler = SARoot.AddComponent<PointerHandler>();
 
+        var pHandler = SARoot.AddComponent<PointerHandler>();
+        pHandler.AddComponent<NearInteractionTouchable>();
         pHandler.OnPointerClicked.AddListener(OnPointerClicked);
     }
 
     private void OnPointerClicked(MixedRealityPointerEventData data)
     {
         var result = data.Pointer.Result;
-        Debug.Log("A sphere spawn at: " + result.StartPoint);
-        SpawnSphere(result.StartPoint);
+        SpawnSphere(result.Details.Point);
     }
 
     private void SpawnSphere(Vector3 spawnPos)
     {
-        var obj = new GameObject();
-        obj.transform.parent = spawnRoot.transform;
-        obj.transform.position = spawnPos;
+        var obj = Instantiate(spawnPrefab, spawnPos, Quaternion.identity, spawnRoot.transform);
+        Debug.Log("A sphere spawn at: " + spawnPos, obj);
     }
 }
